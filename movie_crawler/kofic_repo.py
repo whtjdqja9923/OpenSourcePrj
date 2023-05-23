4e75d46c612707b868d66efc2fc6127dd7abd801
sbcho <whtjdqja9923@naver.com>
2023-05-23 13:44:19 +0900

    DB ddl문 작성

--- movie_crawler/kofic_repo.py
+++ movie_crawler/kofic_repo.py
@@ -1,4 +1,8 @@
 from dataclasses import dataclass, field
+import sqlite3
+
+db_path = "./share/"
+db_name = "movie_data.db"
 
 #데이터 예시
 #{"movieCd":"20211567","movieNm":"수프와 이데올로기","movieNmEn":"Soup and Ideology",
@@ -41,5 +45,62 @@ class people_data:
     poeple_name: str = ''
     people_name_eng: str = ''
     rep_role_name: str = ''
-    filmo_names: str = ''
+    filmo_names: list[str] = field(default_factory=list)
 
+#테이블 조회 후 없으면 필요테이블 생성
+def create_table_movie():
+    con = sqlite3.connect(db_path + db_name)
+    cursor = con.cursor()
+
+    ddl_movie_basic = ''' CREATE TABLE movie_basic ( 
+        "movie code"         VARCHAR(255) NOT NULL  PRIMARY KEY  ,
+        "movie name"         VARCHAR(255) NOT NULL    ,
+        "movie name eng"     VARCHAR(255)     ,
+        "prdt year"          VARCHAR(4)     ,
+        "open date"          VARCHAR(8) NOT NULL    ,
+        "type name"          VARCHAR(255)     ,
+        "prdt stat name"     VARCHAR(255)     ,
+        "rep nation name"    VARCHAR(255) NOT NULL    ,
+        "rep genre name"     VARCHAR(255) NOT NULL    
+        ); '''
+    ddl_companys = ''' CREATE TABLE companys ( 
+        "movie code"         VARCHAR(255) NOT NULL    ,
+        "company code"       VARCHAR(255) NOT NULL    ,
+        "company name"       VARCHAR(255)     ,
+        CONSTRAINT pk2 PRIMARY KEY ( "movie code", "company code" ),
+        FOREIGN KEY ( "movie code" ) REFERENCES movie_basic( "movie code" )  
+        ); '''
+    ddl_directors = ''' CREATE TABLE directors ( 
+        "director num"       INTEGER NOT NULL  PRIMARY KEY  ,
+        "movie code"         VARCHAR(255) NOT NULL    ,
+        "people name"        VARCHAR(255)     ,
+        FOREIGN KEY ( "movie code" ) REFERENCES movie_basic( "movie code" )  
+        ); '''
+    ddl_people_basic = ''' CREATE TABLE people_basic ( 
+        "people code"        VARCHAR(255) NOT NULL  PRIMARY KEY  ,
+        "people name"        VARCHAR(255) NOT NULL    ,
+        "people name eng"    VARCHAR(255)     ,
+        "rep role name"      VARCHAR(255) NOT NULL  
+        ); '''
+    ddl_people_filmo = ''' CREATE TABLE people_filmo ( 
+        "filmo num"          INTEGER NOT NULL  PRIMARY KEY  ,
+        "people code"        VARCHAR(255) NOT NULL    ,
+        "filmo name"         VARCHAR(255) NOT NULL    ,
+        FOREIGN KEY ( "people code" ) REFERENCES people_basic( "people code" )  
+        ); '''
+        
+    if not cursor.execute('''select name from sqlite_master where type="table" and name="movie_basic"''').fetchall():
+        cursor.execute(ddl_movie_basic)
+    if not cursor.execute('''select name from sqlite_master where type="table" and name="companys"''').fetchall():
+        cursor.execute(ddl_companys)
+    if not cursor.execute('''select name from sqlite_master where type="table" and name="directors"''').fetchall():
+        cursor.execute(ddl_directors)
+    if not cursor.execute('''select name from sqlite_master where type="table" and name="people_basic"''').fetchall():
+        cursor.execute(ddl_people_basic)
+    if not cursor.execute('''select name from sqlite_master where type="table" and name="people_filmo"''').fetchall():
+        cursor.execute(ddl_people_filmo)
+
+    con.commit()
+
+    cursor.close()
+    con.close()
