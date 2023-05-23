de79356a26d17fe122f16fc92eda1c48ce12b05b
sbcho <whtjdqja9923@naver.com>
2023-05-23 23:38:20 +0900

    db 저장 추가

--- movie_crawler/kofic_crawl.py
+++ movie_crawler/kofic_crawl.py
@@ -1,11 +1,10 @@
 from time import sleep
+import json
 
 import requests
 from bs4 import BeautifulSoup
-import json
-from kofic_repo import movie_data, directors, companys, people_data
-import datetime
-from datetime import datetime
+
+from kofic_repo import movie_data, directors, companys, people_data, save_movie_list, save_people_list
 
 api_key = "1786ab654f52342e30fcd571ce235d0f"
 kofic_api_url = {"searchMovieList":"http://kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json?", \
@@ -19,7 +18,7 @@ class kofic_crawler:
             curPage = str(i)
             webpage = ''
             ext = 0
-            data = movie_data()
+            data_list = [];
 
             while(webpage == ''):
                 try:
@@ -28,7 +27,7 @@ class kofic_crawler:
                         break
                     webpage = requests.get(kofic_api_url['searchMovieList'] + "key=" + api_key + \
                                         "&" + "itemPerPage=" + self.itemPerPage + \
-                                            "&" + "curPage=" + curPage)
+                                        "&" + "curPage=" + curPage)
                 except:
                     sleep(5)
             
@@ -37,6 +36,8 @@ class kofic_crawler:
             movie_list = crawl_result.get('movieListResult').get('movieList')
 
             for movie in movie_list:
+                data = movie_data()
+
                 data.movie_code = movie.get('movieCd')
                 data.movie_name = movie.get('movieNm')
                 data.movie_name_eng = movie.get('movieNmEn')
@@ -59,14 +60,15 @@ class kofic_crawler:
                     company.company_name = c.get('companyNm')
                     data.companys.append(company)
 
+                data_list.append(data)
 
-            sleep(0.5) # 과부하 방지
+            save_movie_list(data_list)
+            # sleep(0.5) # 과부하 방지
 
     def searchPeopleList_crawl(self, START_PAGE, END_PAGE):
         for i in range (START_PAGE, END_PAGE+1):
-            curPage = i
-            curPage = str(curPage)
-            data = people_data()
+            curPage = str(i)
+            data_list = []
 
             webpage = ''
             ext = 0
@@ -76,7 +78,7 @@ class kofic_crawler:
                     if ext == 30: #최대 시도 횟수
                         break
                     webpage = requests.get(kofic_api_url['searchPeopleList'] + "key=" + api_key + \
-                                           "&" + "itemPerPage=" + self.itemPerPage + \
+                                            "&" + "itemPerPage=" + self.itemPerPage + \
                                             "&" + "curPage=" + curPage)
                 except:
                     sleep(5)
@@ -86,10 +88,16 @@ class kofic_crawler:
             people_list = crawl_result.get('peopleListResult').get('peopleList')
 
             for people in people_list:
+                data = people_data()
+
                 data.people_code = people.get('peopleCd')
                 data.poeple_name = people.get('peopleNm')
                 data.people_name_eng = people.get('peopleNmEn')
                 data.rep_role_name = people.get('repRoleNm')
-                data.filmo_names = people.get('filmoNames')
+                if people.get('filmoNames'):
+                    data.filmo_names = people.get('filmoNames').split('|')
+                
+                data_list.append(data)
 
-            sleep(0.5) # 과부하 방지
+            save_people_list(data_list)
+            # sleep(0.5) # 과부하 방지
