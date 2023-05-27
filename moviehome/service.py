7171acfbde756a90b824e51b676e932ad49b6222
whtjdqja9923 <127500549+whtjdqja9923@users.noreply.github.com>
2023-05-27 16:25:25 +0900

    로그인 서비스 구현

--- moviehome/service.py
+++ moviehome/service.py
@@ -1,6 +1,20 @@
 from moviehome.repo import member
 from moviehome.repo import get_member, save_member
 
+def login(m:member):
+    compare = get_member(m)
+    result = {"flag":True, "msg":""}
+
+    #아이디가 존재하지 않거나 비밀번호가 맞지 않으면
+    if (not compare):
+        result["flag"] = False
+        result["msg"] = "아이디가 존재하지 않습니다"
+    elif m.password != compare[0].password:
+        result["flag"] = False
+        result["msg"] = "비밀번호가 일치하지 않습니다"
+    
+    return result
+
 def register(m:member, password_confirm):
     result = {"flag":False, "msg":"등록에 실패하였습니다"}
 
