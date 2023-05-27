03ba2fd75dd410bf05ac3616d44c1cfdaf70b006
whtjdqja9923 <127500549+whtjdqja9923@users.noreply.github.com>
2023-05-27 16:22:11 +0900

    로그인/회원변경 폼 작성

--- moviehome/forms.py
+++ moviehome/forms.py
@@ -9,3 +9,13 @@ class signup_form(FlaskForm):
     gender = SelectField('성별', choices=[('M', '남성'), ('F', '여성')])
     email = EmailField('이메일', validators=[Email(email_validator, '이메일을 확인해주세요')], render_kw={"placeholder": "이메일을 입력하세요(example@chungbuk.ac.kr)"})
     age = StringField('나이', validators=[DataRequired('나이를 확인해주세요')], render_kw={"placeholder": "나이를 입력하세요"})
+    
+class signin_form(FlaskForm):
+    member_id = StringField('아이디', validators=[DataRequired('아이디를 확인해주세요')], render_kw={"placeholder": "아이디를 입력하세요"})
+    password = PasswordField('비밀번호', validators=[DataRequired('비밀번호를 확인해주세요')], render_kw={"placeholder": "비밀번호를 입력하세요"})
+
+class memberupdate_form(FlaskForm):
+    password = PasswordField('비밀번호', validators=[DataRequired('비밀번호를 확인해주세요')], render_kw={"placeholder": "비밀번호를 입력하세요"})
+    gender = SelectField('성별', choices=[('M', '남성'), ('F', '여성')])
+    email = EmailField('이메일', validators=[Email(email_validator, '이메일을 확인해주세요')], render_kw={"placeholder": "이메일을 입력하세요(example@chungbuk.ac.kr)"})
+    age = StringField('나이', validators=[DataRequired('나이를 확인해주세요')], render_kw={"placeholder": "나이를 입력하세요"})
