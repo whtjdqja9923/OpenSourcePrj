021269c1a1ee2c02e981e41ca90af78f1945b3b9
overline3021 <overline3021@gmail.com>
2023-05-30 20:35:37 +0900

    함수 수정

--- movie-recommendation/recommendation.py
+++ movie-recommendation/recommendation.py
@@ -4,15 +4,25 @@ from . import movie_util
 movies_bp = Blueprint('movies_bp', __name__, url_prefix='/', template_folder="templates")
 
 
-# 추천 영화목록 출력
+# weighted rating 기준 영화추천 페이지
 @movies_bp.route('/movies')
-def movie_detail():
-    # 사용자 정보에 따라 추천 영화목록 반환
-    return render_template('movie_recommendation_list.html', movie_recommendation_list=movie_util.get_movie_recommendation_list())
+def movie_recommendation_list():
+    # weighted rating 기준 상위 10개 영화 반환
+    return render_template('movie_recommendation_list.html', top_n_movie=movie_util.get_top_n_movies_by_weighted_rating(10))
 
 
-# 영화 정보 및 연관 영화 출력
-@movies_bp.route('/movies/<int:movie_id>')
-def movie_detail(movie_id):
-    # 선택한 영화의 movie_id에 해당하는 영화 정보 및 연관 영화 반환
-    return render_template('movie_detail.html', movie_title=movie_util.get_title_by_id(movie_id), movie_rating=movie_util.get_rating_by_id(movie_id))
+# 장르별 영화추천 페이지
+@movies_bp.route('/movies/<string:genre>')
+def movies_by_genre(genre):
+    # 장르별 weightedRating 기준 상위 n개 영화 출력
+    return render_template('movies_by_genre.html', genre=genre, top_n_movies=movie_util.get_top_n_movies_by_genre(genre, 10))
+
+
+# 영화 상세정보 페이지
+@movies_bp.route('/movies/<int:movieCd>')
+def movie_detail(movieCd):
+
+    actors, movieRating, prdtYear, openDt, directors, repGenreNm, movieNmEng= movie_util.get_movie_details(movieCd)
+
+    # 선택한 영화의 movieCd에 해당하는 영화 정보 및 유사한 영화목록 반환
+    return render_template('movie_detail.html', movieNmEng=movieNmEng, movieRating=movieRating, prdtYear=prdtYear, openDt=openDt, repGenreNm=repGenreNm, actors=actors, directors=directors)
