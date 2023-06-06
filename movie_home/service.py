0eb36312bc59708400856b04089885e31a675fd7
whtjdqja9923 <127500549+whtjdqja9923@users.noreply.github.com>
2023-06-06 18:47:52 +0900

    movielist 서비스 작성

--- movie_home/service.py
+++ movie_home/service.py
@@ -1,5 +1,6 @@
 from movie_home.repo import member
 from movie_home.repo import get_member, save_member
+import movie_crawler as mv
 
 def login(m:member):
     compare = get_member(m)
@@ -47,3 +48,59 @@ def update_member_info(m:member):
         return result
 
     return result
+
+def movie_list(col_num=3, keyword="", m_id="", per_page=12, offset=0):
+    m_code = "None"
+    if m_id:
+        mem = get_member(member(member_id=m_id))
+        m_code = mem[0].member_code
+    
+    if not keyword:
+        movies = mv.get_movie_list_audience_desc(m_code)
+    else:
+        basic = mv.get_movie(movie_name=keyword)
+        rating = []
+        user_rating = []
+        
+        user = mv.get_rating(code=m_code, type="member")
+        for i in range(len(basic)):
+            rating.append((mv.get_rating(code=basic[i].movie_code, type="movie")[0]))
+            for j in range(len(user)):
+                flag = False
+                if basic[i].movie_code == user[j].movie_code:
+                    user_rating.append(user[j])
+                    flag = True
+                    break
+            if not flag:
+                user_rating.append(mv.movie_rating())
+            
+        movies = list(zip(basic, rating, user_rating))
+        
+    m = []
+    for movie in movies:
+        if movie[0].show_time:
+            movie[0].show_time = (str(int(int(movie[0].show_time)/60)) + "시간 " + str(int(int(movie[0].show_time)%60))+"분")
+        m.append({
+            "movie_code":movie[0].movie_code,
+            "movie_name":movie[0].movie_name,
+            "prdt_year":movie[0].prdt_year,
+            "type_name":movie[0].type_name,
+            "rep_genre_name":movie[0].rep_genre_name,
+            "show_time":movie[0].show_time,
+            "poster_img_link":movie[0].poster_img_link,
+            "audience_num":movie[0].audience_num,
+            "score":movie[1].score,
+            "max_score":movie[1].max_score,
+            "source":movie[1].source,
+            "rating_count":movie[1].rating_count,
+            "user_rating":movie[2].score,
+            "user_max_rating":movie[2].max_score
+        })
+        
+    if keyword:
+        m = sorted(m, key=(lambda x : int(x['audience_num']) if x['audience_num'] is not None else 0), reverse=True)    
+    result = []
+    for i in range(0, len(m), col_num):
+        result.append(m[i:i+col_num])
+        
+    return result[offset:offset+per_page], len(result)
