1d6ed2325c5202ccae008c272d7c900f6b7af377
overline3021 <overline3021@gmail.com>
2023-06-09 12:57:39 +0900

    유사도 계산 방식 수정

--- movie-recommendation/similarity.py
+++ movie-recommendation/similarity.py
@@ -1,21 +1,20 @@
 import sqlite3
 import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
 db_path = "./share/"
 db_name = "database.db"
 similarity_matrix_file = "./share/similarity_matrix.npy"
 
 
-def get_movie_data():
+def get_movie_synopses():
     con = sqlite3.connect(db_path + db_name)
-    con.row_factory = sqlite3.Row
     cursor = con.cursor()
 
-    query = '''SELECT mb."prdt year", mb."type name", mb."rep nation name", mb."rep genre name", c."company code", d."director num"
-               FROM movie_basic AS mb
-               LEFT JOIN companys AS c ON mb."movie code" = c."movie code"
-               LEFT JOIN directors AS d ON mb."movie code" = d."movie code"
-               ORDER BY mb."movie code" ASC
+    query = '''SELECT "movie code", synopsis
+               FROM movie_detail
+               ORDER BY "movie code" ASC
     '''
 
     cursor.execute(query)
@@ -26,30 +25,27 @@ def get_movie_data():
     return result
 
 
-def calculate_similarity(movie_data):
-    num_movies = len(movie_data)
+def calculate_similarity_matrix(movie_synopses):
+    num_movies = len(movie_synopses)
+
+    synopses = [synopsis for _, synopsis in movie_synopses]
+
+    vectorizer = TfidfVectorizer()
+
+    synopses = [synopsis if synopsis is not None else '' for synopsis in synopses]
+
+    tfidf_matrix = vectorizer.fit_transform(synopses)
+
+    similarity_matrix = cosine_similarity(tfidf_matrix, tfidf_matrix)
     
-    similarity_matrix = np.zeros((num_movies, num_movies))
-    
-    for i in range(num_movies):
-        for j in range(i + 1, num_movies):
-            
-            movie1 = movie_data[i]
-            movie2 = movie_data[j]
-            count = 0.0
-            for i in range(6):
-                if movie1[i] == movie2[i]:
-                    count += 1.0
-            similarity = count / 6.0
-            similarity_matrix[i][j] = similarity
-            similarity_matrix[j][i] = similarity
+    similarity_matrix = np.where(np.eye(num_movies), 0, similarity_matrix)
 
     return similarity_matrix
 
 
 if __name__ == '__main__':
-    movies = get_movie_data()
+    movie_synopses = get_movie_synopses()
 
-    similarity_matrix = calculate_similarity(movies)
+    similarity_matrix = calculate_similarity_matrix(movie_synopses)
 
     np.save(similarity_matrix_file, similarity_matrix)
